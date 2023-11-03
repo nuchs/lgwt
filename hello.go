891bd7136dd3c475e9c329ae1a8623cafc8c1179
nuchs <sjbrown@live.co.uk>
2023-11-03 14:30:04 +0000

    Add default greeting

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const englishGreetingPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name + "!"
+	if name == "" {
+		name = "World"
+	}
+	return englishGreetingPrefix + name + "!"
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,22 @@ package main
 
 import "testing"
 
-func TestHellow(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris!"
+func TestHello(t *testing.T) {
+	t.Run("Say hello to someone", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris!"
+		assertEqual(t, got, want)
+	})
+	t.Run("Say 'Hello, World!' by default", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+		assertEqual(t, got, want)
+	})
+}
 
+func assertEqual(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
