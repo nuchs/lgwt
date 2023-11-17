32af49bcc75ce3ae76f9471ddb1ef6d17944acb8
nuchs <sjbrown@live.co.uk>
2023-11-17 11:22:45 +0000

    Racer v1

---
+++ selector/selector_test.go
@@ -0,0 +1,31 @@
+package selector
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20)
+	fastServer := makeDelayedServer(0)
+	defer slowServer.Close()
+	defer fastServer.Close()
+	slowUrl := slowServer.URL
+	fastUrl := fastServer.URL
+
+	want := fastUrl
+	got := Racer(slowUrl, fastUrl)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
