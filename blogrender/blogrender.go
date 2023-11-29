8fc6ff2fbba36fa4dfd1ca7908e4a978b80a0c06
nuchs <sjbrown@live.co.uk>
2023-11-29 20:07:19 +0000

    Initial render of blog post in html

--- blogrender/blogrender.go
+++ blogrender/blogrender.go
@@ -1,1 +1,39 @@
 package blogrender
+
+import (
+	"fmt"
+	"io"
+)
+
+type Post struct {
+	Title       string
+	Description string
+	Body        string
+	Tags        []string
+}
+
+func Render(w io.Writer, p Post) error {
+	_, err := fmt.Fprintf(w, "<h1>%s</h1>\n<p>%s</p>", p.Title, p.Description)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "\nTags: <ul>")
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range p.Tags {
+		_, err := fmt.Fprintf(w, "<li>%s</li>", tag)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = fmt.Fprintf(w, "</ul>")
+	if err != nil {
+		return err
+	}
+
+	return err
+}
--- blogrender/blogrender_test.go
+++ blogrender/blogrender_test.go
@@ -1,1 +1,33 @@
 package blogrender_test
+
+import (
+	"bytes"
+	"lgwt/blogrender"
+	"testing"
+)
+
+func TestRender(t *testing.T) {
+	aPost := blogrender.Post{
+		Title:       "Hello, World!",
+		Body:        "This is a post",
+		Description: "This is a description",
+		Tags:        []string{"go", "tdd"},
+	}
+
+	t.Run("Single post to HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blogrender.Render(&buf, aPost)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<h1>Hello, World!</h1>
+<p>This is a description</p>
+Tags: <ul><li>go</li><li>tdd</li></ul>`
+		if got != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+
+	})
+}
